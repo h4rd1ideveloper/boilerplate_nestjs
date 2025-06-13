b04b15505667fa0f0552674cdb0c2ad84155fa05
yan <h4rd1i@gmail.com>
2025-06-13 10:40:49 -0300

    Add CRUD structure for `Products` and `Companies` modules with TypeORM integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CompaniesModule } from './companies/companies.module';
 import { ProductsModule } from './products/products.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Company } from './companies/entities/company.entity';
+import { Product } from './products/entities/product.entity';
 
 @Module({
-  imports: [CompaniesModule, ProductsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [Company, Product],
+      synchronize: true,
+    }),
+    CompaniesModule,
+    ProductsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
